1b061eb476d4c2c28fb6c54a80fa35cd087f82c2
Feyipitan Igbowu <feyiigbowu@gmail.com>
2023-01-27 15:10:22 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+#You are going to write a List Comprehension to create a new list called squared_numbers. This new list should contain every number in the list numbers but each number should be squared.
+#numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+
+#squared_numbers = [number * number for number in numbers]
+#print(squared_numbers)
+
+
+## 2)finding even numbers
+
+#numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+#even_number = [num for num in numbers if num % 2 == 0]
+#print(even_number)
+
+# 3) data overlap
+#file_1 = [1, 2, 3]
+#file_2 = [2, 3, 4]
+#result = [int(num) for num in file_1 if num in file_2]
+#print(result)
+
